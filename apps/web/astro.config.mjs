3633ef6de42f09c998a235c441280367b04e6ef0
lucaArchidiacono <mail@archiluc.ch>
2025-10-02 21:54:20 +0200

    fixes

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -3,9 +3,6 @@ import tailwind from "@astrojs/tailwind";
 /** @type {import('astro').AstroUserConfig} */
 export default {
   integrations: [tailwind()],
-  server: { port: Number(process.env.PORT) || 4321 },
-  experimental: {
-    middleware: true
-  }
+  server: { port: Number(process.env.PORT) || 4321 }
 };
 
